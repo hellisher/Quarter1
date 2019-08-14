474e9bfaf983ea91b2591ca88a2b4592f8cf4d71
Valerii El-Khatib <elkhatibvm@gmail.com>
2019-08-14 23:14:08 +0300

    Add Lesson 3

---
+++ 190803 Lesson1.Homework/Friends/FriendsCell.swift
@@ -0,0 +1,26 @@
+//
+//  FriendsCell.swift
+//  190803 Lesson1.Homework
+//
+//  Created by Валерий Эль-Хатиб on 14/08/2019.
+//  Copyright © 2019 EVM Corporation. All rights reserved.
+//
+
+import UIKit
+
+class FriendsCell: UITableViewCell {
+
+    @IBOutlet weak var friendName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ 190803 Lesson1.Homework/Groups/GroupsCell.swift
@@ -0,0 +1,26 @@
+//
+//  GroupsCell.swift
+//  190803 Lesson1.Homework
+//
+//  Created by Валерий Эль-Хатиб on 14/08/2019.
+//  Copyright © 2019 EVM Corporation. All rights reserved.
+//
+
+import UIKit
+
+class GroupsCell: UITableViewCell {
+
+    @IBOutlet weak var groupName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
