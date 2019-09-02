06e5e8e1dff796651ac041b569edeba677f6a304
Valerii El-Khatib <elkhatibvm@gmail.com>
2019-09-02 18:18:39 +0300

    Сделал отображение количеств лайков, просмотров и т.д. на экране новостей

--- 190803 Lesson1.Homework/Friends/FriendsCell.swift
+++ 190803 Lesson1.Homework/Friends/FriendsCell.swift
@@ -14,7 +14,6 @@ class FriendsCell: UITableViewCell {
     @IBOutlet weak var friendPhoto: UIImageView!
     @IBOutlet weak var friendShadow: UIView!
     
-    //thinking
     func animateAuthButton() {
         let animation = CASpringAnimation(keyPath: "transform.scale")
         animation.fromValue = 0
