f432f498980a78bc9891188d59103563cac836ab
Valerii El-Khatib <elkhatibvm@gmail.com>
2019-09-15 11:22:14 +0300

    Add Session

---
+++ 190803 Lesson1.Homework/Classes/Session.swift
@@ -0,0 +1,23 @@
+//
+//  Session.swift
+//  190803 Lesson1.Homework
+//
+//  Created by Валерий Эль-Хатиб on 15/09/2019.
+//  Copyright © 2019 EVM Corporation. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Session {
+    
+    static let instance = Session()
+    
+    private init() {}
+    
+    var myName: String = ""
+    var token: String = ""
+    var userId: Int = 0
+    var myImage: UIImage = UIImage(named: "Valerii El-Khatib")!
+
+}
